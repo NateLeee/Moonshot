1a6bcfaa0735b2b2eaec9db244538df17582fec2
Nate Lee <nateleee.dev@gmail.com>
2020-07-13 12:39:43 +0800

    Better file structure.

---
+++ Moonshot/Models/Astronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Nate Lee on 7/13/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable {
+    var id: String
+    var name: String
+    var description: String
+}
+
