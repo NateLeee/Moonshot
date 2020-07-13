2312ae74562f5ea46f7b67008832810fb5a11380
Nate Lee <nateleee.dev@gmail.com>
2020-07-13 12:52:54 +0800

    Added an extension file: Bundle-Decodable

---
+++ Moonshot/Extensions/Bundle-Decodable.swift
@@ -0,0 +1,13 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Nate Lee on 7/13/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    
+}
