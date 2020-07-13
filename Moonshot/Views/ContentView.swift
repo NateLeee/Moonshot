63027f26e7a7b32c0e141a7ae99e0744ffeabed6
Nate Lee <nateleee.dev@gmail.com>
2020-07-13 12:35:31 +0800

    Added resources & assets.

---
+++ Moonshot/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Nate Lee on 7/13/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    var body: some View {
+        Button("Decode JSON") {
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
