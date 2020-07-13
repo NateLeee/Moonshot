a4c1cc05866c9a4496af4d24dd5727b12ebba704
Nate Lee <nateleee.dev@gmail.com>
2020-07-13 13:44:42 +0800

    Added a model file: Mission.

---
+++ Moonshot/Models/Mission.swift
@@ -0,0 +1,21 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Nate Lee on 7/13/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct Crew: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: String
+    let crew: [Crew]
+    let description: String
+    let launchDate: Date?
+}
