67374c4012adc90e5d05875f80963cd7de07bbaa
Nate Lee <nateleee.dev@gmail.com>
2020-07-13 16:05:11 +0800

    Added a MissionView file.

---
+++ Moonshot/Views/MissionView.swift
@@ -0,0 +1,21 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Nate Lee on 7/13/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionView()
+    }
+}
